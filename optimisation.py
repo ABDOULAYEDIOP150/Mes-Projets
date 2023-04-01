43283712f9e4c5285d14c5a44d619692a6758107
ABDOULAYE DIOP <117550162+ABDOULAYEDIOP150@users.noreply.github.com>
2023-04-01 21:35:30 +0200

    Update optimisation.py

--- optimisation.py
+++ optimisation.py
@@ -1,11 +1,3 @@
-#%%
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct  1 18:49:52 2022
-
-@author: EL ALLAKI otmane
-"""
 
 import matplotlib.pyplot as plt 
 import numpy as np
